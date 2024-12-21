f874cac511c48ef4e2df0ce8fd10e0bea1d831c3
bwhite <bwhite@globalmed.com>
2024-12-21 15:41:21 -0700

    day 9

--- AdventOfCode2024/Day8.cs
+++ AdventOfCode2024/Day8.cs
@@ -43,21 +43,129 @@ namespace AdventOfCode2024
                     {
                         continue;
                     }
-                    if(foundChars.ContainsKey(c))
+                    if (foundChars.ContainsKey(c))
                     {
-                        foreach(Coordinate coordinate in foundChars[c])
+                        foreach (Coordinate coordinate in foundChars[c])
                         {
                             //calculate two antinodes
+                            int dx = coordinate.X - j;
+                            int dy = coordinate.Y - i;
+
+                            Coordinate antiNode1 = new Coordinate(coordinate.X + dx, coordinate.Y + dy);
+                            Coordinate antiNode2 = new Coordinate(j - dx, i - dy);
+
                             //check to see if in bounds of map
                             //check to see if antinode already exists at that coordinate
+                            if (antiNode1.X >= 0 && antiNode1.X <= rightBound && antiNode1.Y >= 0 && antiNode1.Y <= bottomBound)
+                            {
+                                foundAntinodes.Add(antiNode1);
+                            }
+                            if (antiNode2.X >= 0 && antiNode2.X <= rightBound && antiNode2.Y >= 0 && antiNode2.Y <= bottomBound)
+                            {
+                                foundAntinodes.Add(antiNode2);
+                            }
                         }
+                        foundChars[c].Add(new Coordinate(j, i));
+                    }
+                    else
+                    {
+                        foundChars[c] = new HashSet<Coordinate> { new Coordinate(j, i) };
                     }
                 }
             }
+            Console.WriteLine(foundAntinodes.Count);
+        }
+
+
+        public void Solve2()
+        {
+            //maintain dictionary of char value and coordinates of the occurrences
+            Dictionary<char, HashSet<Coordinate>> foundChars = new Dictionary<char, HashSet<Coordinate>>();
+            //also maintain hashset of the found coordinates of antinodes
+            HashSet<Coordinate> foundAntinodes = new HashSet<Coordinate>();
+
+            int rightBound = Grid[0].Count - 1;
+            int bottomBound = Grid.Count - 1;
+            for (int i = 0; i < Grid.Count; i++)
+            {
+                for (int j = 0; j < Grid[i].Count; j++)
+                {
+                    char c = Grid[i][j];
+                    if ('.'.Equals(c))
+                    {
+                        continue;
+                    }
+                    if (foundChars.ContainsKey(c))
+                    {
+                        foreach (Coordinate coordinate in foundChars[c])
+                        {
+                            //calculate two antinodes
+                            int dx = coordinate.X - j;
+                            int dy = coordinate.Y - i;
+
+
+                            int greatestCommonDenominator = gcd(dx, dy);
+                            dx = dx / greatestCommonDenominator;
+                            dy = dy / greatestCommonDenominator;
+
+                            int x1 = coordinate.X;
+                            int y1 = coordinate.Y;
+                            foundAntinodes.Add(new Coordinate(x1, y1));
+                            while(x1 >= 0 && x1 <= rightBound && y1 >= 0 && y1 <= bottomBound)
+                            {
+                                foundAntinodes.Add(new Coordinate(x1, y1));
+                                x1 += dx;
+                                y1 += dy;
+                            }
+                            int x2 = coordinate.X;
+                            int y2 = coordinate.Y;
+                            
+                            while(x2 >= 0 && x2 <= rightBound && y2 >= 0 && y2 <= bottomBound)
+                            {
+                                foundAntinodes.Add(new Coordinate(x2, y2));
+                                x2 -= dx;
+                                y2 -= dy;
+                            }
+      
+                        }
+                        foundChars[c].Add(new Coordinate(j, i));
+                    }
+                    else
+                    {
+                        foundChars[c] = new HashSet<Coordinate> { new Coordinate(j, i) };
+                    }
+                }
+            }
+            Console.WriteLine(foundAntinodes.Count);
+        }
+
+        private int gcd (int a, int b)
+        {
+            if (a == 0)
+                return b;
+            return gcd(b % a, a);
         }
 
         private class Coordinate
         {
+            public override int GetHashCode()
+            {
+                string str = $"{X}{Y}";
+                return int.Parse(str);
+            }
+            public override bool Equals(object? obj)
+            {
+                if(obj is Coordinate c)
+                {
+                    return X == c.X && Y == c.Y;
+                }
+                return false;
+            }
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
             public int X;
             public int Y; //frosting was here
 
@@ -80,6 +188,6 @@ namespace AdventOfCode2024
          * 
          * 
          * 
-         *//
+         */
     }
 }
--- AdventOfCode2024/Day9.cs
+++ AdventOfCode2024/Day9.cs
@@ -31,7 +31,36 @@ namespace AdventOfCode2024
                     id++;
                 }
             }
-            Console.WriteLine("test");
+        }
+
+        public void Solve1()
+        {
+            int i = fileBlock.Count - 1;
+            while(i >= 0)
+            {
+                string  currentData = fileBlock[i];
+                if (currentData != ".")
+                {
+                    int firstEmptySpotPtr = fileBlock.IndexOf(".");
+                    if(firstEmptySpotPtr < i)
+                    {
+                        fileBlock[firstEmptySpotPtr] = currentData;
+                        fileBlock[i] = ".";
+                    }
+                }
+                i--;
+            }
+            long rVal = 0;
+            for(i = 0; i < fileBlock.Count; i++)
+            {
+                string fileData = fileBlock[i];
+                if (fileData.Equals("."))
+                {
+                    continue;
+                }
+                rVal += int.Parse(fileData) * i;
+            }
+            Console.WriteLine(rVal);
         }
     }
 }
